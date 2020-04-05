a4722855a4aa406677e533d707a5d7aa7a5e3596
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-05 17:54:59 +0200

    comparison logical operator and regular expression

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -11,7 +11,10 @@ const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: {type: Date, default: Date.now},
+  date: {
+    type: Date,
+    default: Date.now
+  },
   isPublished: Boolean
 });
 
@@ -20,9 +23,76 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model('Course', courseSchema);
 
-const course = new Course({
-  name: 'Node.js course',
-  author: 'Marceau',
-  tags: ['node', 'backend'],
-  isPublished: false
-});
+async function createCourse() {
+  const course = new Course({
+    name: 'angular course',
+    author: 'Marceau',
+    tags: ['angular', 'frontend'],
+    isPublished: false
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+// async function getCourses() {
+//   //query operators
+//   //eq (equal)
+//   //neq (not equal)
+//   //gt (greater than)
+//   //gte(greater than or equal to)
+//   //lt (less than)
+//   //lte(less than or equal to)
+//   //in
+//   //nin ( not in)
+//   const courses = await Course
+//   // .find({ author: 'Marceau', isPublished: false })
+//   // .find({ price: {$gt: 10, $lte: 20}}) //filter all courses by price greater than 10
+//   .find({price: {$in: [10,15,20]}})
+//   .limit(10)
+//   .sort({name: 1}) // 1 for ascending order, -1 for descending order
+//   .select({name: 1, tags: 1});
+//  console.log(courses);
+
+//  // logical operators
+//  //and
+//  //or
+//   const courses2 = await Course
+//   .find()
+//   .or({ author: 'Marceau'}, {isPublished: false })
+//     .and({ author: 'Marceau' }, { isPublished: false })
+//   .limit(10)
+//   .sort({name: 1}) // 1 for ascending order, -1 for descending order
+//   .select({name: 1, tags: 1});
+//  console.log(courses);
+
+//  //regular expression
+//  const courses3 = await Course
+//  //starts with
+//  .find({author: /^Marceau/})
+//  //ends with
+//  .find({ author: /Tassin$/i }) // i for case unsensistive
+//  //contains
+// .find({author: /.*Marc.*/i})
+//  .limit(10)
+//  .sort({name: 1}) // 1 for ascending order, -1 for descending order
+//  .select({name: 1, tags: 1});
+// console.log(courses);
+
+// }
+
+async function getCourses() {
+  const courses = await Course
+    .find({
+      author: /.*Marceau.*/
+    })
+    //  .limit(10)
+    .sort({
+      name: 1
+    }) // 1 for ascending order, -1 for descending order
+    .count();
+  console.log(courses);
+
+}
+
+getCourses();
