dcaf423c6050046f198dc8449eba5d454a5edb30
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-11 15:43:57 +0200

    move routes from index to routes

--- vidly-api-project-mongoDB/index.js
+++ vidly-api-project-mongoDB/index.js
@@ -1,24 +1,16 @@
 const winston = require('winston');
 require('winston-mongodb');
 require('express-async-errors');
-const error = require('./middleware/error');
 const config = require('config');
 const startupDebugger = require('debug')('app:startup');
 const dbDebugger = require('debug')('app:db');
-const helmet = require('helmet');
 const morgan = require('morgan');
-const genres = require('./routes/genres'); // call router methods from courses file
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
-const home = require('./routes/home');
-const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
 const Joi = require("joi");
 Joi.objectId = require('joi-objectid')(Joi); // usefull to validate id before submitting request
+const express = require('express');
+const app = express();
+require('./startup/routes')(app);
 
 //manual version
 // process.on('uncaughtException', (ex) => {
@@ -63,24 +55,6 @@ mongoose.connect('mongodb://localhost/vidly-api-project')
 
 app.set('view engine', 'pug');
 app.set('views', './views'); //default
-
-app.use(express.json()); //middleware function => req.body
-app.use(express.urlencoded({
-  extended: true
-})); //middleware function => req.body
-app.use(express.static('public')); // Define a folder for static web components
-app.use(helmet()); // Define HTTP headers
-app.use('/api/genres', genres); // for any routes beginning by /api/genres, use routes frome genres
-app.use('/', home);
-app.use('/api/genres', genres); // for any routes beginning by /api/genres, use routes frome genres
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-app.use(error);
-
-
 
 if (app.get('env') === "development") {
   app.use(morgan('tiny')); // Define logs
---
+++ vidly-api-project-mongoDB/startup/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const helmet = require('helmet');
+const genres = require('../routes/genres'); // call router methods from courses file
+const customers = require('../routes/customers');
+const movies = require('../routes/movies');
+const rentals = require('../routes/rentals');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
+const home = require('../routes/home');
+const error = require('../middleware/error');
+
+module.exports = function (app) {
+  app.use(express.json()); //middleware function => req.body
+  app.use(express.urlencoded({ extended: true })); //middleware function => req.body
+  app.use(express.static('public')); // Define a folder for static web components
+  app.use(helmet()); // Define HTTP headers
+  app.use('/api/genres', genres); // for any routes beginning by /api/genres, use routes frome genres
+  app.use('/', home);
+  app.use('/api/genres', genres); // for any routes beginning by /api/genres, use routes frome genres
+  app.use('/api/customers', customers);
+  app.use('/api/movies', movies);
+  app.use('/api/rentals', rentals);
+  app.use('/api/users', users);
+  app.use('/api/auth', auth);
+  app.use(error);
+};
