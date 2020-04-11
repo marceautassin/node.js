ea6fdd1c099e3e43865ed1bc78849bc04774001e
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-11 17:58:02 +0200

    unit test on number string and array

---
+++ testing-demo/tests/lib.test.js
@@ -0,0 +1,33 @@
+const lib = require('../lib');
+
+describe('absolute', () => {
+  it('should return a positive number if input is positive', () => {
+  const result = lib.absolute(1);
+  expect(result).toBe(1);
+  });
+
+  it('should return a positive number if input is negative', () => {
+  const result = lib.absolute(-1);
+  expect(result).toBe(1);
+  });
+
+  it('should return 0 if input is 0', () => {
+  const result = lib.absolute(0);
+  expect(result).toBe(0);
+  });
+});
+
+describe('greet', () => {
+it('should return the greeting message', () => {
+  const result = lib.greet('Marceau');
+  expect(result).toMatch(/Marceau/);
+  expect(result).toContain('Marceau');
+})
+});
+
+describe('getCurrencies', () => {
+  it('should return supported currencies', () => {
+    const result = lib.getCurrencies();
+    expect(result).toEqual(expect.arrayContaining(['EUR', 'USD', 'AUD']))
+  });
+});
