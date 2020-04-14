d9b497a0d9331702e469db192d07ed7ebb0c43d8
Marceau Tassin <marceau.tassin@gmail.com>
2020-04-14 19:58:35 +0200

    TDD 404 on rental

--- vidly-api-project-mongoDB/routes/returns.js
+++ vidly-api-project-mongoDB/routes/returns.js
@@ -1,8 +1,17 @@
+const {Rental} = require('../models/rental');
 const express = require('express');
 const router = express.Router(); // same as app but for routes
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  if(!req.body.customerId) return res.status(400).send('CustomerId not provided');
+  if (!req.body.movieId) return res.status(400).send('CustomerId not provided');
+  const rental = await Rental.findOne({
+    'customerId._id': req.body.customerId, // use '' property to access key in subdocument
+    'movieId._id': req.body.movieId});
+  if(!rental) return res.status(404).send('This rental does not exist');
+
   res.status(401).send('unauthorized');
+
 });
 
 module.exports = router;
--- vidly-api-project-mongoDB/tests/integration/returns.test.js
+++ vidly-api-project-mongoDB/tests/integration/returns.test.js
@@ -1,53 +1,91 @@
-const {Rental} = require ('../../models/rental');
-const {User} = require ('../../models/user');
-const mongoose = require('mongoose');
-const request = require('supertest');
 
-describe('POST /returns', () => {
+const request = require('supertest');
+const {
+  Rental
+} = require('../../models/rental');
+const {
+  Movie
+} = require('../../models/movie');
+const {
+  User
+} = require('../../models/user');
+const mongoose = require('mongoose');
+
+describe('/api/returns', () => {
   let server;
   let customerId;
   let movieId;
   let rental;
+  let token;
 
-    beforeEach(async () => {
-      server = require('../../index');
-      customerId = mongoose.Types.ObjectId();
-      movieId = mongoose.Types.ObjectId();
-      token = new User().generateAuthToken();
-
-      rental = new Rental({
-        customer: {
-          _id: customerId,
-          name: '12345',
-          phone: '12345'
-        },
-        movie: {
-          _id: movieId,
-          title: '12345',
-          dailyRentalRate: 2
-        }
+  const exec = () => {
+    return request(server)
+      .post('/api/returns')
+      .set('x-auth-token', token)
+      .send({
+        customerId,
+        movieId
       });
-      await rental.save();
-    });
+  };
 
-    afterEach(async () => {
-      await Rental.remove({});
-      await server.close();
-    });
+  beforeEach(async () => {
+    server = require('../../index');
 
-    it('should work', async () => {
-      const result = await Rental.findById(rental._id);
-      expect(result).not.toBeNull();
+    customerId = mongoose.Types.ObjectId();
+    movieId = mongoose.Types.ObjectId();
+    token = new User().generateAuthToken();
+
+
+    rental = new Rental({
+      customer: {
+        _id: customerId,
+        name: '12345',
+        phone: '12345'
+      },
+      movie: {
+        _id: movieId,
+        title: '12345',
+        dailyRentalRate: 2
+      }
+    });
+    await rental.save();
   });
 
-    it('should return 401 if client is not logged in', async () => {
-      const res = await request(server)
-        .post('/api/returns')
-        .send({
-          customerId,
-          movieId
-        });
+  afterEach(async () => {
+    await server.close();
+    await Rental.remove({});
+  });
 
-      expect(res.status).toBe(401);
-    });
+  it('should return 401 if client is not logged in', async () => {
+    token = '';
+
+    const res = await exec();
+
+    expect(res.status).toBe(401);
+  });
+
+  it('should return 400 if customerId is not provided', async () => {
+    customerId = '';
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it('should return 400 if movieId is not provided', async () => {
+    movieId = '';
+
+    const res = await exec();
+
+    expect(res.status).toBe(400);
+  });
+
+  it('should return 404 if no rental found for the customer/movie', async () => {
+    await Rental.remove({});
+
+    const res = await exec();
+
+    expect(res.status).toBe(404);
+  });
+
 });
